1e35f3520e795a8d66d9fb3d6af3e9760b65f5d4
azx19 <azx19.5984@gmail.com>
2023-05-31 20:12:34 +0200

    pep tx extra serialization and verification unit test

--- tests/unit_tests/pepenet_social.cpp
+++ tests/unit_tests/pepenet_social.cpp
@@ -31,6 +31,8 @@
 #include "gtest/gtest.h"
 
 #include "pepenet_social/pepenet_social.h"
+#include "cryptonote_basic/cryptonote_basic.h"
+#include "cryptonote_basic/cryptonote_format_utils.h"
 
 #define GTEST_COUT std::cerr << "[          ] [ INFO ]"
 
@@ -60,4 +62,47 @@ TEST(pepenet_social, sing_verify_msg_1)
   ASSERT_TRUE(sign_msg(msg, sig, pk, sk));
   //check msg
   ASSERT_TRUE(check_msg_sig(msg, sig, pk));
+}
+
+TEST(pepenet_social, pep_to_from_extra)
+{
+  //fake transfer_main arguments
+  std::string pep = "Long live pepent. Freedom to everyone!!!";
+  std::string pseudonym = "Mr. pepe";
+  std::string sk_seed = "1498b5467a63dffa2dc9d9e069caf075d16fc33fdd4c3b01bfadae6433767d93";
+  crypto::hash tx_ref = crypto::cn_fast_hash(pseudonym.data(), pseudonym.size());
+  //add to tx extra
+  cryptonote::transaction tx;
+  std::string lzma_pep;
+  ASSERT_TRUE(lzma_compress_msg(pep, lzma_pep));
+  //get keys
+  crypto::public_key pk;
+  crypto::secret_key sk;
+  ASSERT_TRUE(secret_key_from_seed(sk_seed, sk));
+  ASSERT_TRUE(crypto::secret_key_to_public_key(sk, pk));
+  crypto::signature full_pep_sig;
+  //contruct full content for signing
+  std::string full_pep = pep + pseudonym + std::string(tx_ref.data, 32) + std::string(pk.data, 32);
+  sign_msg(full_pep, full_pep_sig, pk, sk);
+  //add all fields to extra
+  ASSERT_TRUE(cryptonote::add_lzma_pep_to_tx_extra(tx.extra, lzma_pep));
+  ASSERT_TRUE(cryptonote::add_pseudonym_to_tx_extra(tx.extra, pseudonym));
+  ASSERT_TRUE(cryptonote::add_eddsa_pubkey_to_tx_extra(tx.extra, pk));
+  ASSERT_TRUE(cryptonote::add_eddsa_signature_to_tx_extra(tx.extra, full_pep_sig));
+  ASSERT_TRUE(cryptonote::add_tx_reference_to_tx_extra(tx.extra, tx_ref));
+  //get all fields from tx extra
+  std::string pep_, lzma_pep_, pseudonym_;
+  crypto::hash tx_ref_;
+  crypto::public_key pk_;
+  crypto::signature full_pep_sig_;
+
+  ASSERT_TRUE(cryptonote::get_lzma_pep_from_tx_extra(tx.extra, lzma_pep_));
+  ASSERT_TRUE(lzma_decompress_msg(lzma_pep_, pep_));
+  ASSERT_TRUE(cryptonote::get_pseudonym_from_tx_extra(tx.extra, pseudonym_));
+  ASSERT_TRUE(cryptonote::get_eddsa_pubkey_from_tx_extra(tx.extra, pk_));
+  ASSERT_TRUE(cryptonote::get_eddsa_signature_from_tx_extra(tx.extra, full_pep_sig_));
+  ASSERT_TRUE(cryptonote::get_tx_reference_from_tx_extra(tx.extra, tx_ref_));
+  //construct full pep for sig. verification
+  std::string full_pep_ = pep_ + pseudonym_ + std::string(tx_ref_.data, 32) + std::string(pk_.data, 32);
+  ASSERT_TRUE(check_msg_sig(full_pep_, full_pep_sig_, pk_));
 }
