43054ce16c3c65d4516f079e41359cc3e8703c4c
azx19 <azx19.5984@gmail.com>
2023-07-25 06:17:07 +0200

    fixed version string check

--- src/rpc/rpc_version_str.cpp
+++ src/rpc/rpc_version_str.cpp
@@ -39,13 +39,14 @@ namespace rpc
 // Expected format of Monero software version string:
 // 1) Four numbers, one to two digits each, separated by periods
 // 2) Optionally, one of the following suffixes:
-//      a) -release
-//      b) -<hash> where <hash> is exactly nine lowercase hex digits
+//      a) -beta
+//      b) -release
+//      c) -<hash> where <hash> is exactly nine lowercase hex digits
 
 bool is_version_string_valid(const std::string& str)
 {
     return std::regex_match(str, std::regex(
-        "^\\d{1,2}(\\.\\d{1,2}){3}(-(release|[0-9a-f]{9}))?$",
+        "^\\d{1,2}(\\.\\d{1,2}){3}(-(beta|release|[0-9a-f]{9}))?$",
         std::regex_constants::nosubs
     ));
 }
